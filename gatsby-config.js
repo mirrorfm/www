4f7fce124bd5662d6778f9877cc118c82027217a
Stephane Bruckert <contact@stephanebruckert.com>
2019-12-24 16:19:45 +0100

    Display channel titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
+const channelType = {
+  host: 'String',
+  channel_name: 'String',
+  channel_id: 'String',
+  upload_playlist_id: 'String',
+  count_tracks: 'Number',
+  thumbnails: {default: {width: 1, url: 'String', height: 1}, high: {width: 1, url: 'String', height: 1}, medium: {width: 1, url: 'String', height: 1}},
+  upload_playlist_id: 'String',
+  count_tracks: 0,
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -30,5 +41,45 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-apiserver",
+      options: {
+        // Type prefix of entities from server
+        typePrefix: "internal__",
+
+        // The url, this should be the endpoint you are attempting to pull data from
+        url: `https://qdfngarl1b.execute-api.eu-west-1.amazonaws.com/get`,
+
+        method: "GET",
+
+        // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+        // using this name. i.e. posts.json
+        name: `channel`,
+
+        // Nested level of entities in response object, example: `data.posts`
+        entityLevel: `youtube.channels`,
+
+        // Define schemaType to normalize blank values
+        // example:
+        // const postType = {
+        //   id: 1,
+        //   name: 'String',
+        //   published: true,
+        //   object: {a: 1, b: '2', c: false},
+        //   array: [{a: 1, b: '2', c: false}]
+        // }
+        schemaType: channelType,
+
+        enableDevRefresh: true,
+
+        // Optionally override key used to re-source data
+        // when `gatsby develop` is running.
+        // Requires `enableDevRefresh: true`.
+        // See setting directly above this one.
+        // See also https://github.com/gatsbyjs/gatsby/issues/14653
+        // Default is `id`
+        auth: false,
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,8 +15,26 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <ul>
+      {data.allInternalChannel.nodes
+        .map(({ channel_name }, index) => (
+          <li key={index}>{channel_name}</li>
+        ))}
+    </ul>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allInternalChannel {
+      nodes {
+        id
+        channel_name
+        channel_id
+      }
+    }
+  }`
