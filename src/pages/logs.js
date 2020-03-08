be701a19f2cd58c2c796a524a4ddf72dc7ba9a74
Stephane Bruckert <contact@stephanebruckert.com>
2020-03-08 13:01:22 +0000

    Add logs page

---
+++ src/pages/logs.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Loader from 'react-loader-spinner'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Link } from "gatsby"
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
+
+
+import Moment from 'react-moment';
+
+function generate(genres) {
+  return Object.entries((genres || {}))
+      .sort((a, b) => (b[1] - a[1]))
+      .slice(0, 4);
+}
+
+class Home extends Component {
+  state = {
+    loading: false,
+    error: false,
+    data: {
+      events: [],
+      total: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchChannels()
+  }
+
+  render() {
+    const { events } = this.state.data;
+    const classes = makeStyles(theme => ({
+      paper: {
+        padding: theme.spacing(2),
+        margin: 'auto',
+        maxWidth: 500,
+      },
+      image: {
+        width: 128,
+        height: 128,
+      },
+      img: {
+        margin: 'auto',
+        display: 'block',
+        maxWidth: '100%',
+        maxHeight: '100%',
+      },
+      root: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        '& > *': {
+          margin: theme.spacing(0.5),
+        },
+      },
+    }));
+
+    return (
+        <Layout>
+          <SEO title="Home" />
+          <div>
+            {this.state.loading ? (
+                <Loader
+                    type="Grid"
+                    color="lightgrey"
+                    height={50}
+                    width={50}
+                    timeout={3000}
+                    style={{ textAlign: "center" }}
+                />
+            ) : this.state.data ? (
+                <>
+                  <p style={{ textAlign: `right` }}>
+                    <Link style={{ fontSize: `60px`, textDecoration: `none` }} to="/add/">+</Link>
+                  </p>
+                  <ul style={{ listStyleType: `none`, marginLeft: `0` }}>
+                    {events.map((e, index) => (
+                        <li style={{ marginBottom: `20px` }} key={index}>
+                          Added {e.added} {Object.keys(e.genres)[0]} tracks to {e.channel_name} <Moment fromNow unix>{e.timestamp}</Moment>
+                        </li>
+                    ))}
+                  </ul>
+                  <span>
+              </span>
+                </>
+            ) : (
+                <p>Oh noes, error fetching logs :(</p>
+            )}
+          </div>
+        </Layout>
+    )
+  }
+
+  // This data is fetched at run time on the client.
+  fetchChannels = () => {
+    this.setState({ loading: true })
+
+    axios
+        .get(`https://qdfngarl1b.execute-api.eu-west-1.amazonaws.com/mirrorfm/events`)
+        .then(({ data }) => {
+          this.setState({
+            loading: false,
+            data
+          })
+        })
+        .catch(error => {
+          this.setState({ loading: false, error })
+        })
+  }
+}
+
+export default Home
